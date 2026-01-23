871465b22f2d2039347535395b66b50a21bdae25
Mayur Wagh <143696773+mayurwagh66@users.noreply.github.com>
2026-01-23 19:24:32 +0530

    Add files via upload

---
+++ PalindromString.java
@@ -0,0 +1,17 @@
+class PalindromString{
+
+    public static boolean Palindrome(String str){
+
+        for(int i=0;i<str.length()/2;i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false; //not a palindrome
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String str = "racecar";
+        System.out.println(Palindrome(str));
+    }
+}
